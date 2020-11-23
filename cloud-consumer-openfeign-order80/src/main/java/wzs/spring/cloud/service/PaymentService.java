c85af0cd1aa458a0b43d98becccb3851e9a43950
wangzhs <915196681@qq.com>
2020-11-23 22:52:26 +0800

    集成openfeign。客户端实现注解方式调取服务端接口

---
+++ cloud-consumer-openfeign-order80/src/main/java/wzs/spring/cloud/service/PaymentService.java
@@ -0,0 +1,21 @@
+package wzs.spring.cloud.service;
+
+import com.wzs.spring.cloud.entities.CommonResult;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+/**
+ * @author wangzhs
+ * @since 23/11/2020
+ */
+@Component
+// value 调取服务的applicationName path类似于 提取共有的url路径
+@FeignClient(value = "cloud-provider-payment", path = "/payment")
+public interface PaymentService {
+
+    @GetMapping("/get/{id}")
+    public CommonResult getPaymentById(@PathVariable("id") Long id);
+
+}
