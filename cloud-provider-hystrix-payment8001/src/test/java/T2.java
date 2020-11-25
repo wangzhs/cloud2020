927ec214e8b17a650f7f7b9efb6a20a88d167e08
wangzhs <915196681@qq.com>
2020-11-25 14:34:22 +0800

    gateway yml文件网关规则生成条件添加

---
+++ cloud-provider-hystrix-payment8001/src/test/java/T2.java
@@ -0,0 +1,12 @@
+import java.time.ZonedDateTime;
+
+/**
+ * @author wangzhs
+ * @since 25/11/2020
+ */
+public class T2 {
+
+    public static void main(String[] args) {
+        System.out.println(ZonedDateTime.now().toString());
+    }
+}
