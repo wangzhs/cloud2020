4db2e6f95ab61d1575aa9a757d119cb83bf1f242
wangzhs <915196681@qq.com>
2020-11-25 13:50:56 +0800

    gateway yml文件使用服务名负载均衡

--- cloud-gateway9527/src/main/java/com/wzs/spring/cloud/config/GatewayConfig.java
+++ cloud-gateway9527/src/main/java/com/wzs/spring/cloud/config/GatewayConfig.java
@@ -12,13 +12,13 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class GatewayConfig {
 
-    @Bean
-    public RouteLocator routeLocator(RouteLocatorBuilder builder) {
-        return builder.routes()
-                // 同等于配置文件
-                .route(r -> r.uri("http://localhost:8001").predicate(p -> p.getRequest().getPath().subPath(0).toString().startsWith("/payment/")))
-//                .route(r -> r.uri("http://news.baidu.com/guoji"))
-                .build();
-    }
+//    @Bean
+//    public RouteLocator routeLocator(RouteLocatorBuilder builder) {
+//        return builder.routes()
+//                // 同等于配置文件
+//                .route(r -> r.uri("http://localhost:8001").predicate(p -> p.getRequest().getPath().subPath(0).toString().startsWith("/payment/")))
+////                .route(r -> r.uri("http://news.baidu.com/guoji"))
+//                .build();
+//    }
 
 }
