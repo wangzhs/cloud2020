023f9f319570c9ccfa71e995a8d9132197e58d17
wangzhs <915196681@qq.com>
2020-11-25 14:59:18 +0800

    gateway 增加全区filter

---
+++ cloud-gateway9527/src/main/java/com/wzs/spring/cloud/config/MyGatewayFilter.java
@@ -0,0 +1,35 @@
+package com.wzs.spring.cloud.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+/**
+ * @author wangzhs
+ * @since 25/11/2020
+ */
+@Component
+@Slf4j
+public class MyGatewayFilter implements GlobalFilter, Ordered {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+
+        String username = exchange.getRequest().getQueryParams().getFirst("username");
+        if ("".equals(username) || null == username) {
+            exchange.getResponse().setStatusCode(HttpStatus.BAD_GATEWAY);
+            return exchange.getResponse().setComplete();
+        }
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
