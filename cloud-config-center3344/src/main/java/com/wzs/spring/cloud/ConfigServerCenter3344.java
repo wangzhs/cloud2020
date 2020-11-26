c6e4aab2a3d9d4debfa067aab6a108f7915e933f
wangzhs <915196681@qq.com>
2020-11-26 11:13:57 +0800

    通过bus准确发送通知到配置文件服务器，然后通知使用配置文件服务器 准确发送

--- cloud-config-center3344/src/main/java/com/wzs/spring/cloud/ConfigServerCenter3344.java
+++ cloud-config-center3344/src/main/java/com/wzs/spring/cloud/ConfigServerCenter3344.java
@@ -8,8 +8,13 @@ import org.springframework.cloud.config.server.EnableConfigServer;
  * @author wangzhs
  * @since 25/11/2020
  */
+
+/**
+ * 刷新需要 curl -XPOST "http://config-3344.com:3344/actuator/bus-refresh"
+ * 定点通知 ：curl -XPOST "http://config-3344.com:3344/actuator/bus-refresh/cloud-config-server-center:3355"
+ */
 @SpringBootApplication
-@EnableConfigServer /*刷新需要 curl -XPOST "http://config-3344.com:3344/actuator/bus-refresh"*/
+@EnableConfigServer
 public class ConfigServerCenter3344 {
 
     public static void main(String[] args) {
